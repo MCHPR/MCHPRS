d53c09d2a6c957480a6f09deed739ff9dea94f52
StackDoubleFlow <ojaslandge@gmail.com>
2020-04-20 04:42:30 -0500

    Log commands

--- src/plot/commands.rs
+++ src/plot/commands.rs
@@ -1,9 +1,11 @@
 use super::Plot;
 use crate::blocks::Block;
 use crate::server::Message;
+use log::info;
 
 impl Plot {
     pub(super) fn handle_command(&mut self, player: usize, command: &str, args: Vec<&str>) {
+        info!("{} issued command: {} {}", self.players[player].username, command, args.join(" "));
         match command {
             "//1" | "//pos1" => {
                 let player = &mut self.players[player];
