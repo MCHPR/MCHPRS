e86e141fedb89914ec68cea11767a17f03f9d445
StackDoubleFlow <ojaslandge@gmail.com>
2021-05-04 08:22:23 -0500

    Add get_name to Block

--- src/blocks/mod.rs
+++ src/blocks/mod.rs
@@ -1071,6 +1071,7 @@ macro_rules! blocks {
                         }
                     ),*
                 },
+                get_name: $get_name:expr,
                 $( solid: $solid:literal, )?
                 $( transparent: $transparent:literal, )?
                 $( cube: $cube:literal, )?
@@ -1162,6 +1163,20 @@ macro_rules! blocks {
                     _ => None,
                 }
             }
+
+            // Not all props will be part of the name
+            #[allow(unused_variables)]
+            pub fn get_name(self) -> &'static str {
+                match self {
+                    $(
+                        Block::$name {
+                            $(
+                                $prop_name,
+                            )*
+                        } => $get_name,
+                    )*
+                }
+            }
         }
     }
 }
@@ -1174,6 +1189,7 @@ blocks! {
         from_names(_name): {
             "air" => {}
         },
+        get_name: "air",
     },
     Glass {
         props: {},
@@ -1182,6 +1198,7 @@ blocks! {
         from_names(_name): {
             "glass" => {}
         },
+        get_name: "glass",
         transparent: true,
         cube: true,
     },
@@ -1212,6 +1229,7 @@ blocks! {
                 wire: Default::default()
             }
         },
+        get_name: "redstone_wire",
     },
     WallSign {
         props: {
@@ -1250,6 +1268,15 @@ blocks! {
                 facing: Default::default()
             }
         },
+        get_name: match sign_type {
+            0 => "oak_wall_sign",
+            1 => "spruce_wall_sign",
+            2 => "birch_wall_sign",
+            3 => "jungle_wall_sign",
+            4 => "acacia_wall_sign",
+            5 => "dark_oak_wall_sign",
+            _ => "invalid_wall_sign"
+        },
     },
     Lever {
         props: {
@@ -1274,6 +1301,7 @@ blocks! {
                 lever: Default::default()
             }
         },
+        get_name: "lever",
     },
     StoneButton {
         props: {
@@ -1294,6 +1322,7 @@ blocks! {
                 button: Default::default()
             }
         },
+        get_name: "stone_button",
     },
     Sign {
         props: {
@@ -1332,6 +1361,15 @@ blocks! {
                 rotation: 0
             }
         },
+        get_name: match sign_type {
+            0 => "oak_sign",
+            1 => "spruce_sign",
+            2 => "birch_sign",
+            3 => "jungle_sign",
+            4 => "acacia_sign",
+            5 => "dark_oak_sign",
+            _ => "invalid_sign"
+        },
     },
     RedstoneTorch {
         props: {
@@ -1351,6 +1389,7 @@ blocks! {
                 lit: true
             }
         },
+        get_name: "redstone_torch",
     },
     RedstoneWallTorch {
         props: {
@@ -1369,6 +1408,7 @@ blocks! {
                 facing: Default::default()
             }
         },
+        get_name: "redstone_wall_torch",
     },
     RedstoneRepeater {
         props: {
@@ -1395,6 +1435,7 @@ blocks! {
                 repeater: Default::default()
             }
         },
+        get_name: "repeater",
     },
     RedstoneLamp {
         props: {
@@ -1414,6 +1455,7 @@ blocks! {
                 lit: false
             }
         },
+        get_name: "redstone_lamp",
         solid: true,
         cube:true,
     },
@@ -1436,6 +1478,7 @@ blocks! {
                 direction: Default::default()
             }
         },
+        get_name: "tripwire_hook",
     },
     RedstoneComparator {
         props: {
@@ -1460,6 +1503,7 @@ blocks! {
                 comparator: Default::default()
             }
         },
+        get_name: "comparator",
     },
     RedstoneBlock {
         props: {},
@@ -1468,6 +1512,7 @@ blocks! {
         from_names(_name): {
             "redstone_block" => {}
         },
+        get_name: "redstone_block",
         transparent: true,
         cube: true,
     },
@@ -1485,6 +1530,7 @@ blocks! {
                 facing: Default::default()
             }
         },
+        get_name: "redstone_block",
         solid: true,
         cube: true,
     },
@@ -1502,6 +1548,7 @@ blocks! {
                 pickles: 1
             }
         },
+        get_name: "sea_pickle",
     },
     Target {
         props: {},
@@ -1510,6 +1557,7 @@ blocks! {
         from_names(_name): {
             "target" => {}
         },
+        get_name: "target",
         solid: true,
         cube: true,
     },
@@ -1520,6 +1568,7 @@ blocks! {
         from_names(_name): {
             "stone_pressure_plate" => {}
         },
+        get_name: "stone_pressure_plate",
     },
     Barrel {
         props: {},
@@ -1528,6 +1577,7 @@ blocks! {
         from_names(_name): {
             "barrel" => {}
         },
+        get_name: "barrel",
         solid: true,
         cube: true,
     },
@@ -1538,6 +1588,7 @@ blocks! {
         from_names(_name): {
             "hopper" => {}
         },
+        get_name: "hopper",
         transparent: true,
         cube: true,
     },
@@ -1548,6 +1599,7 @@ blocks! {
         from_names(_name): {
             "sandstone" => {}
         },
+        get_name: "sandstone",
         solid: true,
         cube: true,
     },
@@ -1558,6 +1610,7 @@ blocks! {
         from_names(_name): {
             "furnace" => {}
         },
+        get_name: "furnace",
         solid: true,
         cube: true,
     },
@@ -1568,6 +1621,7 @@ blocks! {
         from_names(_name): {
             "quartz_block" => {}
         },
+        get_name: "quartz_block",
         solid: true,
         cube: true,
     },
@@ -1578,6 +1632,7 @@ blocks! {
         from_names(_name): {
             "smooth_stone_slab" => {}
         },
+        get_name: "smooth_stone_slab",
         transparent: true,
         cube: true,
     },
@@ -1588,6 +1643,7 @@ blocks! {
         from_names(_name): {
             "quartz_slab" => {}
         },
+        get_name: "quartz_slab",
         transparent: true,
         cube: true,
     },
@@ -1618,6 +1674,7 @@ blocks! {
             "red_concrete" => { color: BlockColorVariant::Red },
             "black_concrete" => { color: BlockColorVariant::Black }
         },
+        get_name: "quartz_slab",
         solid: true,
         cube: true,
     },
@@ -1648,6 +1705,24 @@ blocks! {
             "red_stained_glass" => { color: BlockColorVariant::Red },
             "black_stained_glass" => { color: BlockColorVariant::Black }
         },
+        get_name: match color {
+            BlockColorVariant::White => "white_terracotta",
+            BlockColorVariant::Orange => "orange_terracotta",
+            BlockColorVariant::Magenta => "magenta_terracotta",
+            BlockColorVariant::LightBlue => "light_blue_terracotta",
+            BlockColorVariant::Yellow => "yellow_terracotta",
+            BlockColorVariant::Lime => "lime_terracotta",
+            BlockColorVariant::Pink => "pink_terracotta",
+            BlockColorVariant::Gray => "gray_terracotta",
+            BlockColorVariant::LightGray => "light_gray_terracotta",
+            BlockColorVariant::Cyan => "cyan_terracotta",
+            BlockColorVariant::Purple => "purple_terracotta",
+            BlockColorVariant::Blue => "blue_terracotta",
+            BlockColorVariant::Brown => "brown_terracotta",
+            BlockColorVariant::Green => "green_terracotta",
+            BlockColorVariant::Red => "red_terracotta",
+            BlockColorVariant::Black => "black_terracotta",
+        },
         transparent: true,
         cube: true,
     },
@@ -1658,6 +1733,7 @@ blocks! {
         from_names(_name): {
             "terracotta" => {}
         },
+        get_name: "terracotta",
         solid: true,
         cube: true,
     },
@@ -1687,6 +1763,24 @@ blocks! {
             "green_terracotta" => { color: BlockColorVariant::Green },
             "red_terracotta" => { color: BlockColorVariant::Red },
             "black_terracotta" => { color: BlockColorVariant::Black }
+        },
+        get_name: match color {
+            BlockColorVariant::White => "white_terracotta",
+            BlockColorVariant::Orange => "orange_terracotta",
+            BlockColorVariant::Magenta => "magenta_terracotta",
+            BlockColorVariant::LightBlue => "light_blue_terracotta",
+            BlockColorVariant::Yellow => "yellow_terracotta",
+            BlockColorVariant::Lime => "lime_terracotta",
+            BlockColorVariant::Pink => "pink_terracotta",
+            BlockColorVariant::Gray => "gray_terracotta",
+            BlockColorVariant::LightGray => "light_gray_terracotta",
+            BlockColorVariant::Cyan => "cyan_terracotta",
+            BlockColorVariant::Purple => "purple_terracotta",
+            BlockColorVariant::Blue => "blue_terracotta",
+            BlockColorVariant::Brown => "brown_terracotta",
+            BlockColorVariant::Green => "green_terracotta",
+            BlockColorVariant::Red => "red_terracotta",
+            BlockColorVariant::Black => "black_terracotta",
         },
         solid: true,
         cube: true,
@@ -1718,6 +1812,24 @@ blocks! {
             "red_wool" => { color: BlockColorVariant::Red },
             "black_wool" => { color: BlockColorVariant::Black }
         },
+        get_name: match color {
+            BlockColorVariant::White => "white_wool",
+            BlockColorVariant::Orange => "orange_wool",
+            BlockColorVariant::Magenta => "magenta_wool",
+            BlockColorVariant::LightBlue => "light_blue_wool",
+            BlockColorVariant::Yellow => "yellow_wool",
+            BlockColorVariant::Lime => "lime_wool",
+            BlockColorVariant::Pink => "pink_wool",
+            BlockColorVariant::Gray => "gray_wool",
+            BlockColorVariant::LightGray => "light_gray_wool",
+            BlockColorVariant::Cyan => "cyan_wool",
+            BlockColorVariant::Purple => "purple_wool",
+            BlockColorVariant::Blue => "blue_wool",
+            BlockColorVariant::Brown => "brown_wool",
+            BlockColorVariant::Green => "green_wool",
+            BlockColorVariant::Red => "red_wool",
+            BlockColorVariant::Black => "black_wool",
+        },
         solid: true,
         cube: true,
     },
@@ -1728,6 +1840,7 @@ blocks! {
         get_id: id,
         from_id(id): _ => { id: id },
         from_names(name): {},
+        get_name: "unknown",
         solid: true,
         cube: true,
     }
