b5373a0a7f2bc3dadcbba10f19979f3012b54722
Bram Otte <bram@otte.biz>
2026-05-03 22:29:46 +0200

    Sort properties by their change interval in the state list

--- crates/block_data_gen/src/main.rs
+++ crates/block_data_gen/src/main.rs
@@ -168,6 +168,19 @@ fn process_block(
             ),
         };
     }
+
+    // Sort properties by their change interval in the state list in ascending order.
+    props.sort_by_cached_key(|prop| {
+        let name = prop.name.as_str();
+        
+        let last = json.states.first().unwrap()
+            .properties.as_ref().unwrap()[name].as_str();
+        
+        let interval = json.states.iter().position(|state| state.properties.as_ref().unwrap()[name] != last).unwrap();
+
+        usize::MAX - interval
+    });
+
     let mut attrs = BlockAttrs::default();
     for attr in attrs_str.split(',').filter(|s| !s.is_empty()) {
         match attr {
